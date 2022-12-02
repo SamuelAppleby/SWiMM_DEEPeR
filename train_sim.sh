0ba7b1a9c2e960cacd8d44be85f7b8b6ed955475
Kirsten Richardson (PGR) <b3024896@18scompl028.ncl.ac.uk>
2022-12-02 18:15:58 +0000

    big fix in train_sim.sh

--- train_sim.sh
+++ train_sim.sh
@@ -1,6 +1,6 @@
 export PIPENV_VENV_IN_PROJECT=1
 pipenv install 
-source .venv/Scripts/activate.bat
+source .venv/bin/activate
 python3 train.py "$@" &
-chmod +x RLSimulation/Builds/Linux/ReinforcementLearningSimulation.x86_64 server 127.0.0.1:60261 debug_conf_dir Configs/data/client_debug_config_build_linux.json network_conf_dir Configs/data/network_config.json
+RLSimulation/Builds/Linux/ReinforcementLearningSimulation.x86_64 server 127.0.0.1:60261 debug_conf_dir Configs/data/client_debug_config_build_linux.json network_conf_dir Configs/data/network_config.json
 exit

de9db5f5d0641a29a857afa8b96b48fc0bca17e6
Kirsten Richardson (PGR) <b3024896@18scompl028.ncl.ac.uk>
2023-02-23 17:24:33 +0000

    Finished debugging new server side code, ready to merge

--- gym_underwater/utils/utils.py
+++ gym_underwater/utils/utils.py
@@ -1,9 +1,13 @@
 import os
 import csv
+import time
 import numpy as np
 from stable_baselines.common import set_global_seeds
 from stable_baselines.bench import Monitor
 from gym_underwater.gym_env import UnderwaterEnv
+
+# Used for saving best model
+best_mean_reward = -np.inf
 
 def make_env(vae, obs, opt_d, max_d, img_scale, debug_logs, log_d, seed):
     """
@@ -116,6 +120,7 @@ def create_callback(algo, save_path, reward_threshold, verbose=1):
         :param _globals: (dict)
         :return: (bool) If False: stop training
         """
+
         global best_mean_reward
         episode_rewards = _locals['episode_rewards']
         if len(episode_rewards[-101:-1]) == 0:
@@ -123,14 +128,16 @@ def create_callback(algo, save_path, reward_threshold, verbose=1):
         else:
             mean_reward = round(float(np.mean(episode_rewards[-101:-1])), 1)
         if mean_reward > best_mean_reward:
+            begin_time = time.time()
             if verbose >= 1:
-                print("Saving best model")
+                print("Saving best model ...")
             _locals['self'].save(os.path.join(save_path, "bestmodel"))
             best_mean_reward = mean_reward
             with open(os.path.join(save_path, "ep_nums_for_best.csv"), 'a') as csv_file:
                 writer = csv.writer(csv_file)
                 writer.writerow([len(episode_rewards)])
                 csv_file.close()
+            print("Model saved, time taken: ", time.time() - begin_time)
         #if mean_reward > reward_threshold:
             #if verbose >= 1:
                 #print("Stopping training because the mean reward {:.2f} is above the threshold {}".format(mean_reward, reward_threshold))

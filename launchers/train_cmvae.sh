37da8d2b012a5c86b126b02a18a1ad95faeb05f7
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2023-03-15 12:18:49 +0000

    Fix bin.

--- launchers/train_cmvae.sh
+++ launchers/train_cmvae.sh
@@ -1,4 +1,4 @@
-source ../.venv/Scripts/activate
+source ../.venv/bin/activate
 cd ../cmvae_scripts
 python3 train_cmvae.py --data_dir /home/b7034806/Repositories/Codebases/SWiMM_DEEPeR/data/vae_training_set/64x64 --n_z 10 --epochs 50
 exit

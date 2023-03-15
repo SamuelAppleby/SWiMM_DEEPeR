9d400d909aef65c2e960fa9f3a5614a5e707485f
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2023-03-15 12:56:00 +0000

    Use local.

--- launchers/train_cmvae.bat
+++ launchers/train_cmvae.bat
@@ -1,4 +1,4 @@
 CALL .\..\.venv\Scripts\activate.bat
 cd ..\cmvae_scripts
-START python train_cmvae.py --data_dir D:\vae_training_set\64x64 --n_z 10 --epochs 50
+START python train_cmvae.py --data_dir C:\Users\sambu\Documents\Repositories\CodeBases\SWiMM_DEEPeR\data\vae_training_set\64x64 --n_z 10 --epochs 50
 EXIT

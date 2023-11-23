d45653f15e03a0dafe296ae3133b8ae1d7f384db
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2023-11-23 14:52:31 +0000

    Fix batch error.

--- launchers/vae_image_gen.bat
+++ launchers/vae_image_gen.bat
@@ -1,3 +1,3 @@
 cd ..\builds\windows
-START SWiMM_DEEPeR.exe mode_vae_gen num_images 1000 ..\..\data\image_evaluation test resolutions 1920 1080 640 360 64 64
+START SWiMM_DEEPeR.exe mode_vae_gen num_images 1000 data_dir ..\..\data\image_validation resolutions 1920 1080 640 360 64 64
 EXIT

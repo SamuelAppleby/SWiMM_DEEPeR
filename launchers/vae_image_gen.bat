14e240fa9556e7cf5978e1d9522418c6bf5cc61b
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2023-12-04 00:22:53 +0000

    New Images

--- launchers/vae_image_gen.bat
+++ launchers/vae_image_gen.bat
@@ -1,3 +1,3 @@
 cd ..\builds\windows
-start SWiMM_DEEPeR.exe mode_vae_gen num_images 270000 data_dir ..\..\data\image_similarity resolutions 64 64
+start SWiMM_DEEPeR.exe mode_vae_gen num_images 1000 data_dir ..\..\data\image_similarity resolutions 64 64
 exit

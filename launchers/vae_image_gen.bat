189851447071a8a53401592cfc630a7a02599baf
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2024-04-04 17:33:57 +0100

    Refactor image generation. Save configuration to JSON.

--- launchers/vae_image_gen.bat
+++ launchers/vae_image_gen.bat
@@ -1,3 +1,3 @@
 cd ..\builds\windows
-SWiMM_DEEPeR.exe modeVAEGen numImages 1000 dataDir ..\..\data\cmvae_test resolutions 64 64 seed 3
+SWiMM_DEEPeR.exe modeVAEGen numImages 1000 dataDir ..\..\data\image_validation resolutions 1920 1080 640 360 64 64 seed 27
 exit

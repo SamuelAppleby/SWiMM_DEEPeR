034bdafaa42bc7085bfaaee616aea41278366b84
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2024-04-25 12:37:26 +0100

    Output timings. Final builds.

---
+++ launchers/linux/vae_data_generation_test.sh
@@ -0,0 +1,3 @@
+cd ../../builds/windows
+SWiMM_DEEPeR.exe modeVAEGen dataDir C:/Users/sambu/Documents/Repositories/CodeBases/SWiMM_DEEPeR/data/cmvae_test_set resolutions 64 64 numImages 30000 seed 5
+exit

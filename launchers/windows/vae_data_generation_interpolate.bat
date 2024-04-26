e64c4e720c079a0094930e7f4e6e4158b6fe0da0
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2024-04-26 16:58:22 +0100

    File rename.

--- launchers/windows/vae_data_generation_interpolate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd ..\..\builds\windows
-.\SWiMM_DEEPeR.exe modeVAEGen vaeInterpolate dataDir C:\Users\sambu\Documents\Repositories\CodeBases\SWiMM_DEEPeR\data\cmvae_interpolation resolutions 64 64 seed 7
-exit

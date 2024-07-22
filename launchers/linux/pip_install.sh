061d587ac5c63b8bd13f6cc5004c29e79215f9aa
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2024-07-22 15:18:34 +0100

    Correct Pytorch installation compatible with CUDA 11.2

--- launchers/linux/pip_install.sh
+++ launchers/linux/pip_install.sh
@@ -2,3 +2,4 @@ cd ../..
 if [ -d ".venv" ]; then rm -Rf .venv; fi
 mkdir .venv
 pipenv install
+pipenv run pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu118
--- launchers/windows/pip_install.bat
+++ launchers/windows/pip_install.bat
@@ -1,4 +1,6 @@
+@echo off
 cd ..\..
 if exist .venv rmdir /S /Q .venv
 mkdir .venv
 pipenv install
+pipenv run pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu118

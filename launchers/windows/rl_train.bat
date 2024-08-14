3a618dd9ca3e3fa609efd8a7654efd2d82266e21
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2024-08-14 20:55:02 +0100

    Ready for PPO training.

--- launchers/windows/rl_train.bat
+++ launchers/windows/rl_train.bat
@@ -3,8 +3,8 @@ set "PYTHONPATH=C:\Users\sambu\Documents\Repositories\CodeBases\SWiMM_DEEPeR;%PY
 call ..\..\.venv\Scripts\activate
 cd ..\..\gym_underwater
 
-set seeds=109
-set algorithm=sac
+set seeds=97 101 103 107 109
+set algorithm=ppo
 set n_envs=1
 set render=human
 

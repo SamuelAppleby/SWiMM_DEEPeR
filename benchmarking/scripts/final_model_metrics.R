bc9b241a7319024b7a0b81377caec3adff2d8a2b
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2024-09-25 16:50:54 +0100

    New R graphs for bigger data.

--- benchmarking/scripts/final_model_metrics.R
+++ benchmarking/scripts/final_model_metrics.R
@@ -38,17 +38,25 @@ for (algo_dir in directory_path) {
     if (algo_dir == "C:\\Users\\sambu\\Downloads\\SWiMM_DEEPeR_old\\data") {
       metric_data$Algorithm <- "SAC_OLD"
     }
+    #TODO Can remove this once new data is obtained
+    else {
+      names(metric_data)[names(metric_data) == 'RError'] <- 'DError'
+      metric_data$Episode[metric_data$Step == 2999] <- metric_data$Episode[metric_data$Step == 2999] - 1
+    }
     
     combined_metric_data <- rbind(combined_metric_data,metric_data)
   }
 }
 
-combined_metric_data$Episode <- NULL
-combined_metric_data$Seed <- NULL
+# combined_metric_data$Episode <- NULL
+# combined_metric_data$Seed <- NULL
+
+agg_step <- aggregate(cbind(OutOfView, MaximumDistance, TargetCollision) ~ Algorithm + Seed + Episode, 
+                      data = combined_metric_data, FUN = max, na.rm = TRUE)
 
 # Sum for specific columns
 sum_data <- aggregate(cbind(OutOfView, MaximumDistance, TargetCollision) ~ Algorithm, 
-                      data = combined_metric_data, FUN = sum, na.rm = TRUE)
+                      data = agg_step, FUN = sum, na.rm = TRUE)
 
 df_long <- sum_data %>%
   pivot_longer(cols = c(OutOfView, MaximumDistance, TargetCollision), names_to = "Variable", values_to = "Value")
@@ -71,41 +79,41 @@ ggplot(df_long, aes(x = Algorithm, y = Value, fill = Variable)) +
 
 # Mean for all other columns
 mean_data <- aggregate(. ~ Algorithm,
-                       data = combined_metric_data[, !(names(combined_metric_data) %in% c("OutOfView", "MaximumDistance", "TargetCollision"))],
+                       data = combined_metric_data[, !(names(combined_metric_data) %in% c("Seed", "Episode", "Step", "OutOfView", "MaximumDistance", "TargetCollision"))],
                        FUN = mean, na.rm = TRUE)
 
 # Merge the results
 combined_metric_data_summary <- merge(sum_data, mean_data, by = c("Algorithm"))
 
 df_long_2 <- combined_metric_data_summary %>%
-  pivot_longer(cols = c(MeanAError, MeanRError, MeanASmoothnessError, MeanDSmoothnessError), names_to = "Variable", values_to = "Value")
+  pivot_longer(cols = c(AError, DError, ASmoothnessError, DSmoothnessError), names_to = "Variable", values_to = "Value")
 
 df_long_2$Algorithm <- factor(df_long_2$Algorithm, levels = c("SAC_OLD", "SAC", "PPO", "TD3"))
 
 increase <- 1
 
-ggplot(subset(df_long_2, Variable %in% c("MeanAError", "MeanRError")), aes(x = Algorithm, y = Value * (10 ^ increase), fill = Variable)) +
+ggplot(subset(df_long_2, Variable %in% c("AError", "DError")), aes(x = Algorithm, y = Value * (10 ^ increase), fill = Variable)) +
   scale_x_discrete(labels = c("SAC_OLD" = expression(SAC[paste(1.0)]),
                               "SAC" = expression(SAC[paste(2.0)])))+
   scale_y_log10(labels = scientific_10_wrapper(y = increase)) +
   geom_bar(stat = "identity", position = position_dodge()) +
   labs(x = "Algorithm", y = "MAE") +
-  scale_fill_brewer(palette = "Set2", name = "Error Type", labels = c("MeanAError" = "A",
-                                                                           "MeanRError" = "D")) +
+  scale_fill_brewer(palette = "Set2", name = "Error Type", labels = c("AError" = "A",
+                                                                           "DError" = "D")) +
   theme(legend.position = "bottom",
         text = element_text(family = "Times New Roman"),
         legend.text = element_text(family = "CAL"))
 
 increase <- 3
 
-ggplot(subset(df_long_2, Variable %in% c("MeanASmoothnessError", "MeanDSmoothnessError")), aes(x = Algorithm, y = Value * (10 ^ increase), fill = Variable)) +
+ggplot(subset(df_long_2, Variable %in% c("ASmoothnessError", "DSmoothnessError")), aes(x = Algorithm, y = Value * (10 ^ increase), fill = Variable)) +
   scale_x_discrete(labels = c("SAC_OLD" = expression(SAC[paste(1.0)]),
                               "SAC" = expression(SAC[paste(2.0)])))+
   scale_y_log10(labels = scientific_10_wrapper(y = increase)) +
   geom_bar(stat = "identity", position = position_dodge()) +
   labs(x = "Algorithm", y = "MAE") +
-  scale_fill_brewer(palette = "Set2", name = "Smoothness Error Type", labels = c("MeanASmoothnessError" = expression(S[A]),
-                                                                  "MeanDSmoothnessError" = expression(S[D]))) +
+  scale_fill_brewer(palette = "Set2", name = "Smoothness Error Type", labels = c("ASmoothnessError" = expression(S[A]),
+                                                                  "DSmoothnessError" = expression(S[D]))) +
   theme(legend.position = "bottom",
         text = element_text(family = "Times New Roman"),
         legend.text = element_text(family = "CAL"))

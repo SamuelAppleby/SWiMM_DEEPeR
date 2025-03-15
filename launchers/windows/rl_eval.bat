cb48524cdea958f605639964c32941f89b25f458
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2025-03-15 18:30:08 +0000

    Ready for final model behaviour experiment.

--- launchers/windows/rl_eval.bat
+++ launchers/windows/rl_eval.bat
@@ -5,21 +5,22 @@ call ..\..\.venv\Scripts\activate
 cd ..\..\gym_underwater
 
 set seeds=113 127 131 137 139
-set algorithms=sac_1 ppo_3 td3_4
+set algorithms=sac_noise_1
 set render=human
 
 for %%x in (%algorithms%) do (
-    for /f "tokens=1,2 delims=_" %%a in ("%%x") do (
+    for /f "tokens=1,2,3 delims=_" %%a in ("%%x") do (
         set n=%%a
-        set c=%%b
+        set s=%%b
+        set l=%%c
     )
 
-    set w=C:\Users\sambu\Documents\Repositories\CodeBases\SWiMM_DEEPeR\models\!n!\!n!_!c!\best_model.zip
+    set w=C:\Users\sambu\Documents\Repositories\CodeBases\SWiMM_DEEPeR\models\!n!_!s!\!n!_!s!_!l!\best_model.zip
 
     for %%s in (%seeds%) do (
         for %%r in (%render%) do (
             python inference.py --seed %%s --algorithm !n! --render %%r --pre_trained_model_path !w! --compute_stats
-            python email_notifier.py --msg %%s
+@REM             python email_notifier.py --msg %%s
         )
     )
 )
